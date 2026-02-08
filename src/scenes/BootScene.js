3bd7dbec2bde43e6aa88da8cdeef4059a053b678
Shubham Rajurpalle <rajurpalleshubham1802@gmail.com>
2026-02-08 22:34:31 +0530

    Remove character and weapon data files; update loading paths in BootScene

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -6,7 +6,7 @@ export default class BootScene extends Phaser.Scene {
   preload() {
     const width = this.cameras.main.width;
     const height = this.cameras.main.height;
-    
+
     // Update loading progress
     this.load.on('progress', (value) => {
       const progressBar = document.getElementById('progress');
@@ -14,17 +14,17 @@ export default class BootScene extends Phaser.Scene {
         progressBar.style.width = (value * 100) + '%';
       }
     });
-    
+
     this.load.on('complete', () => {
       const loading = document.getElementById('loading');
       if (loading) {
         loading.style.display = 'none';
       }
     });
-    
+
     // Load data files
-    this.load.json('characters', 'src/data/characters.json');
-    this.load.json('weapons', 'src/data/weapons.json');
+    this.load.json('characters', '/data/characters.json');
+    this.load.json('weapons', '/data/weapons.json');
   }
 
   create() {
